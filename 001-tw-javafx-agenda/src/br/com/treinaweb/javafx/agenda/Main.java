c2f55b441fe5c43ce2082ea83b265d8515b07d0b
PauloAlves8039 <pj.alves547@gmail.com>
2019-12-29 13:48:11 -0300

    Commit inicial do repositório

---
+++ 001-tw-javafx-agenda/src/br/com/treinaweb/javafx/agenda/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Arquivo: Classe - Main
+ * Autor: Paulo Alves
+ * Descrição: utilizada como entry point(ponto de entrada) para execução da aplicação. 
+ * Data: 29/12/2019
+*/
+
+package br.com.treinaweb.javafx.agenda;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ 001-tw-javafx-agenda/src/br/com/treinaweb/javafx/agenda/MainController.java
@@ -0,0 +1,12 @@
+/*
+ * Arquivo: Classe - MainController
+ * Autor: Paulo Alves
+ * Descrição: utilizada como controlador para o entry point.
+ * Data: 29/12/2019
+*/
+
+package br.com.treinaweb.javafx.agenda;
+
+public class MainController {
+	
+}
