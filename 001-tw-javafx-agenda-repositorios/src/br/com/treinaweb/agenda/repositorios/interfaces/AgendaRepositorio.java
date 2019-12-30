64882b4e91e9efcb742438eaba9d66ec64be5dfe
PauloAlves8039 <pj.alves547@gmail.com>
2019-12-30 15:45:07 -0300

    Adicionando interface para a regra de negócio

---
+++ 001-tw-javafx-agenda-repositorios/src/br/com/treinaweb/agenda/repositorios/interfaces/AgendaRepositorio.java
@@ -0,0 +1,18 @@
+/*
+ * Arquivo: interface - AgendaRepositorio
+ * Autor: Paulo Alves
+ * Descrição: responsável por métodos genéricos referentes a regra de negócio da aplicação
+ * Data: 30/12/2019
+*/
+
+package br.com.treinaweb.agenda.repositorios.interfaces;
+
+import java.util.List;
+
+public interface AgendaRepositorio<T> {
+	
+	List<T> selecionar();
+	void inserir(T entidade);
+	void atualizar(T entidade);
+	void excluir(T entidade);
+}
