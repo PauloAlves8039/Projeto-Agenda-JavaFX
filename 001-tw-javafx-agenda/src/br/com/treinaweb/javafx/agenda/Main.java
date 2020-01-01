db3d2fae9cf1aa55b5f4a10431a0a369ba1c05cf
PauloAlves8039 <pj.alves547@gmail.com>
2019-12-31 22:04:58 -0300

    Atribuindo a exibição de contato

--- 001-tw-javafx-agenda/src/br/com/treinaweb/javafx/agenda/Main.java
+++ 001-tw-javafx-agenda/src/br/com/treinaweb/javafx/agenda/Main.java
@@ -10,7 +10,7 @@ package br.com.treinaweb.javafx.agenda;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.fxml.FXMLLoader;
 
 
@@ -18,8 +18,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- 001-tw-javafx-agenda/src/br/com/treinaweb/javafx/agenda/MainController.java
+++ 001-tw-javafx-agenda/src/br/com/treinaweb/javafx/agenda/MainController.java
@@ -1,19 +1,29 @@
 /*
  * Arquivo: Classe - MainController
  * Autor: Paulo Alves
- * Descrição: utilizada como controlador para o entry point.
+ * Descrição: responsável pela comunicação entre código Java e arquivo FXML.
  * Data: 29/12/2019
 */
 
 package br.com.treinaweb.javafx.agenda;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
 import br.com.treinaweb.agenda.entidades.Contato;
+import br.com.treinaweb.agenda.repositorios.impl.ContatoRepositorio;
+import br.com.treinaweb.agenda.repositorios.interfaces.AgendaRepositorio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class MainController {
+public class MainController implements Initializable {
 	
 	@FXML
 	private TableView<Contato> tabelaContatos;
@@ -33,5 +43,25 @@ public class MainController {
 	private Button botaoSalvar;
 	@FXML
 	private Button botaoCancelar;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		this.tabelaContatos.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		carregarTabelaContatos();
+	}
+	
+	private void carregarTabelaContatos() {
+		AgendaRepositorio<Contato> repositorioContato = new ContatoRepositorio();
+		List<Contato> contatos = repositorioContato.selecionar();
+		if(contatos.isEmpty()) {
+			Contato contato = new Contato();
+			contato.setNome("Paulo");
+			contato.setIdade(25);
+			contato.setTelefone("(81)3333-4444");
+			contatos.add(contato);
+		}
+		ObservableList<Contato> contatosObservableList = FXCollections.observableArrayList(contatos);
+		this.tabelaContatos.getItems().setAll(contatosObservableList);
+	}
 	
 }
