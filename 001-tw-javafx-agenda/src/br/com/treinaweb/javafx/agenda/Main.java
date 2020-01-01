682af726dec73660b43243da8538af5d88721871
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-01 13:37:14 -0300

    Ajustando Layout

--- 001-tw-javafx-agenda/src/br/com/treinaweb/javafx/agenda/Main.java
+++ 001-tw-javafx-agenda/src/br/com/treinaweb/javafx/agenda/Main.java
@@ -22,6 +22,8 @@ public class Main extends Application {
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Minha Agenda");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
