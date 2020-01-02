0300a9cd62193d43a79e6bf007c54a9d751a6148
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-02 13:28:35 -0300

    Criando conexão com o banco de dados

---
+++ 001-tw-javafx-agenda-repositorios/src/br/com/treinaweb/agenda/repositorios/impl/ContatoRepositorioJdbc.java
@@ -0,0 +1,56 @@
+/*
+ * Arquivo: Classe - ContatoRepositorioJdbc
+ * Autor: Paulo Alves
+ * Descrição: responsável por implementar métodos da interface AgendaRepositorio para conexão com o banco de dados
+ * Data: 02/01/2020
+*/
+
+package br.com.treinaweb.agenda.repositorios.impl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.treinaweb.agenda.entidades.Contato;
+import br.com.treinaweb.agenda.repositorios.interfaces.AgendaRepositorio;
+
+public class ContatoRepositorioJdbc implements AgendaRepositorio<Contato> {
+
+	@Override
+	public List<Contato> selecionar() throws SQLException {
+		Connection conexao = null;
+		List<Contato> contatos = new ArrayList<Contato>();
+		conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/tw_jdbc", "root", "root");
+		Statement comando = conexao.createStatement();
+		ResultSet rs = comando.executeQuery("SELECT * FROM contatos");
+		while(rs.next()) {
+			Contato contato = new Contato();
+			contato.setId(rs.getInt("id"));
+			contato.setNome(rs.getString("nome"));
+			contato.setIdade(rs.getInt("idade"));
+			contato.setTelefone(rs.getString("telefone"));
+			contatos.add(contato);
+		}
+		return contatos;
+	}
+
+	@Override
+	public void inserir(Contato entidade) {
+				
+	}
+
+	@Override
+	public void atualizar(Contato entidade) {
+				
+	}
+
+	@Override
+	public void excluir(Contato entidade) {
+				
+	}
+
+}
