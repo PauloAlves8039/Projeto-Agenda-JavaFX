74447c8ff5d8f2347055da8c4f81874cba072564
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-04 10:49:50 -0300

    Adicionando arquivo de propriedades

--- 001-tw-javafx-agenda-repositorios/src/br/com/treinaweb/agenda/repositorios/impl/ContatoRepositorioJdbc.java
+++ 001-tw-javafx-agenda-repositorios/src/br/com/treinaweb/agenda/repositorios/impl/ContatoRepositorioJdbc.java
@@ -7,6 +7,9 @@
 
 package br.com.treinaweb.agenda.repositorios.impl;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -14,6 +17,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import br.com.treinaweb.agenda.entidades.Contato;
 import br.com.treinaweb.agenda.repositorios.interfaces.AgendaRepositorio;
@@ -21,11 +25,17 @@ import br.com.treinaweb.agenda.repositorios.interfaces.AgendaRepositorio;
 public class ContatoRepositorioJdbc implements AgendaRepositorio<Contato> {
 
 	@Override
-	public List<Contato> selecionar() throws SQLException {
+	public List<Contato> selecionar() throws SQLException, IOException {
 		Connection conexao = null;
 		List<Contato> contatos = new ArrayList<Contato>();
 		try {
-			conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/tw_jdbc?useTimezone=true&serverTimezone=UTC", "root", "root");
+			Properties props = new Properties();
+			InputStream is = this.getClass().getClassLoader().getResourceAsStream("application.properties");
+			if (is == null) {
+				throw new FileNotFoundException("O arquivo de configuração do banco de dados não foi encontrado.");
+			}
+			props.load(is);
+			conexao = DriverManager.getConnection(props.getProperty("urlConexao"), props.getProperty("usuarioConexao"), props.getProperty("senhaConexao"));
 			Statement comando = conexao.createStatement();
 			ResultSet rs = comando.executeQuery("SELECT * FROM contatos");
 			while (rs.next()) {
@@ -37,7 +47,7 @@ public class ContatoRepositorioJdbc implements AgendaRepositorio<Contato> {
 				contatos.add(contato);
 			}
 		} finally {
-			if(conexao != null) {
+			if (conexao != null) {
 				conexao.close();
 			}
 		}
